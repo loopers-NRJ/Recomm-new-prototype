65d271bcaf3edfb522a3e5bd1b34e869077f22b0
Jayakarthik-jk <karthick72002@gmail.com>
2023-08-02 22:35:56 +0530

    bug fix
    removed the prisma extension

--- src/database/client.ts
+++ src/database/client.ts
@@ -7,17 +7,6 @@ import {
 
 const instance = new PrismaClient();
 
-instance.$extends({
-  query: {
-    room: {
-      create: async ({ query, args }) => {
-        const room = await query(args);
-        room.highestBid = room.product?.price;
-        return room;
-      },
-    },
-  },
-});
 export default instance;
 
 export const matchError = (
