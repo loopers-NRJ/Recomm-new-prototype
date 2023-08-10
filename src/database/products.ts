5a23c5584bf4ba5d9bd93d00d58e2daa03c7c9b6
Jayakarthik-jk <karthick72002@gmail.com>
2023-08-10 21:28:30 +0530

    bug fix

--- src/database/products.ts
+++ src/database/products.ts
@@ -18,7 +18,12 @@ export const productItems = {
   buyerId: true,
   buyer: true,
   modelId: true,
-  model: true,
+  model: {
+    include: {
+      brand: true,
+      categories: true,
+    },
+  },
   ownerId: true,
   owner: true,
   createdAt: true,
@@ -105,7 +110,7 @@ export const createProduct = async ({
   price,
   description,
   pictures,
-  duration,
+  endTime,
 }: CreateProductProps): Promise<Product | ServerError> => {
   const { error } = createProductValidator.validate({
     userId,
@@ -113,10 +118,17 @@ export const createProduct = async ({
     price,
     description,
     pictures,
-    duration,
+    endTime,
   });
+  const endTimeDate = new Date(endTime);
   if (error != null) {
     return new ServerError(error.message, 400);
+  }
+  if (isNaN(endTimeDate.getTime())) {
+    return new ServerError("Invalid end time", 400);
+  }
+  if (endTimeDate.getTime() < Date.now()) {
+    return new ServerError("End time must be after the current time", 400);
   }
   try {
     const model = await client.model.findUnique({
@@ -145,7 +157,7 @@ export const createProduct = async ({
         pictures,
         room: {
           create: {
-            duration,
+            closedAt: endTime,
           },
         },
       },
