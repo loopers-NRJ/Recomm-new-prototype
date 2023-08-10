5a73cc927cf2125dfa9d143863bb55ea8f153120
Jayakarthik-jk <karthick72002@gmail.com>
2023-08-10 21:13:23 +0530

    removed test files

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testTimeout: 60 * 1000, // 1000ms = 1 second
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
-};
